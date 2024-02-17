f59a565614c4a729eda8c670c002c570a5bc0556
besubasi <besubasi@gmail.com>
2024-02-17 11:46:11 +0300

    tur ve tur kategori ekranları

--- src/app/modules/guide-assistant/tour/tourcategory/component/tour-category-list/tour-category-list.component.ts
+++ src/app/modules/guide-assistant/tour/tourcategory/component/tour-category-list/tour-category-list.component.ts
@@ -1,9 +1,16 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {CardModule} from "primeng/card";
+import {TableModule} from "primeng/table";
+import {DropdownModule} from "primeng/dropdown";
 
 @Component({
     selector: 'app-tour-category-list',
     standalone: true,
-    imports: [],
+    imports: [
+        CardModule,
+        TableModule,
+        DropdownModule
+    ],
     templateUrl: './tour-category-list.component.html',
     styleUrl: './tour-category-list.component.scss'
 })
--- src/app/modules/guide-assistant/tour/tourcategory/model/tour-category-model.ts
+++ src/app/modules/guide-assistant/tour/tourcategory/model/tour-category-model.ts
@@ -1,22 +1,12 @@
-import {TourCategorySearchModel} from "./tour-category-search-model";
+import {JsonObject, JsonProperty} from 'json2typescript';
 
+@JsonObject('TourCategoryModel')
 export class TourCategoryModel {
 
-    apiPackage: string;
-    apiName: string;
-    tableName: string;
+    @JsonProperty('companyId', Number, true)
+    companyId: number = null;
 
-    createConstant: boolean = true;
-    createEntity: boolean = true;
-    createModel: boolean = true;
-    createSearchModel: boolean = true;
-    createMapper: boolean = true;
-    createMapperImpl: boolean = true;
-    createRepository: boolean = true;
-    createService: boolean = true;
-    createServiceImpl: boolean = true;
-    createRestController: boolean = true;
-
-    propertyList: TourCategorySearchModel[] = [];
+    @JsonProperty('name', String, true)
+    name: string = null;
 
 }
